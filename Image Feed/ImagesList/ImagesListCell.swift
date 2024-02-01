ffc1e8b33f2eb78c41785e1de3f3401627ef5961
NeX0Zz <den.nickol1406@gmail.com>
2024-02-01 19:24:48 +0300

    Last

--- Image Feed/ImagesList/ImagesListCell.swift
+++ Image Feed/ImagesList/ImagesListCell.swift
@@ -3,6 +3,6 @@ import UIKit
 final class ImagesListCell: UITableViewCell {
     @IBOutlet weak var cellImage: UIImageView!
     @IBOutlet weak var likeButton: UIButton!
-    static let reuseIdentifier = "ImagesListCell"
     @IBOutlet weak var dateLabel: UILabel!
+    static let reuseIdentifier = "ImagesListCell"
 }
