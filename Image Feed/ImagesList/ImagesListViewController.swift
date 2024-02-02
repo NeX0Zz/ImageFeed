0ccd4e7a268744f93a96ee59b296bffa56053eed
NeX0Zz <den.nickol1406@gmail.com>
2024-02-02 10:52:14 +0300

    p

--- Image Feed/ImagesList/ImagesListViewController.swift
+++ Image Feed/ImagesList/ImagesListViewController.swift
@@ -61,7 +61,9 @@ extension ImagesListViewController {
 }
 
 extension ImagesListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         guard let image = UIImage(named: photosName[indexPath.row]) else {
---
+++ Image Feed/ProfileViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var logoutButton: UIButton!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var loginNameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+}
