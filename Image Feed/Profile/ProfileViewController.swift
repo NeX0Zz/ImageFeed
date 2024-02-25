97218080fd042e9c677b72cfe9368f0654b152d4
NeX0Zz <den.nickol1406@gmail.com>
2024-02-25 14:54:23 +0300

    Lastest!

--- Image Feed/Profile/ProfileViewController.swift
+++ Image Feed/Profile/ProfileViewController.swift
@@ -1,11 +1,88 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
-    @IBOutlet private var avatarImageView: UIImageView!
-    @IBOutlet private var nameLabel: UILabel!
-    @IBOutlet private var loginNameLabel: UILabel!
-    @IBOutlet private var descriptionLabel: UILabel!
-    @IBOutlet private var logoutButton: UIButton!
-    @IBAction private func didTapLogoutButton() {
+    
+    private var label: UILabel?
+    private var label2: UILabel?
+    private var label3: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView()
+        
+    }
+    
+    func imageView(){
+        let profileImage = UIImage(named: "avatar")
+        let imageView = UIImageView(image: profileImage)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        
+        imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        
+        let label = UILabel()
+        label.text = "Екатерина Новикова"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = UIColor(named: "YP White")
+        label.font = UIFont.systemFont(ofSize: 23, weight: .bold)
+        
+        view.addSubview(label)
+        
+        label.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
+        label.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 18).isActive = true
+        self.label = label
+        
+        
+        let label2 = UILabel()
+        label2.text = "@ekaterina_nov"
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.textColor = UIColor(named: "YP Gray")
+        label2.font = UIFont.systemFont(ofSize: 13, weight: .bold)
+        
+        view.addSubview(label2)
+        
+        label2.leadingAnchor.constraint(equalTo: label.leadingAnchor).isActive = true
+        label2.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 18).isActive = true
+        self.label2 = label2
+        
+        let label3 = UILabel()
+        label3.text = "Hello, world!"
+        label3.translatesAutoresizingMaskIntoConstraints = false
+        label3.textColor = UIColor(named: "YP White")
+        label3.font = UIFont.systemFont(ofSize: 13, weight: .bold)
+        
+        view.addSubview(label3)
+        
+        label3.leadingAnchor.constraint(equalTo: label2.leadingAnchor).isActive = true
+        label3.topAnchor.constraint(equalTo: label2.bottomAnchor, constant: 18).isActive = true
+        self.label3 = label3
+        
+        
+        
+        let button = UIButton.systemButton(
+            with: UIImage(systemName: "ipad.and.arrow.forward")!,
+            target: self,
+            action: #selector(Self.didTapButton)
+        )
+        
+        button.tintColor = .red
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+        button.centerYAnchor.constraint(equalTo: imageView.centerYAnchor).isActive = true
+    }
+    
+    @objc
+    private func didTapButton() {
+        for view in view.subviews {
+            if view is UILabel {
+                view.removeFromSuperview()
+            }
+        }
     }
 }
+
--- Image Feed/SingleImageView/SingleImageViewController.swift
+++ Image Feed/SingleImageView/SingleImageViewController.swift
@@ -1,21 +1,81 @@
 import UIKit
 
 final class SingleImageViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet private weak var scrollView: UIScrollView!
+    @IBOutlet private var imageView: UIImageView!
+    
+    // MARK: - Properties
+    
     var image: UIImage! {
         didSet {
             guard isViewLoaded else { return }
             imageView.image = image
+            rescaleAndCenterImageInScrollView(image: image)
         }
     }
-
-    @IBOutlet private var imageView: UIImageView!
-
+    
+    // MARK: - Overrides funcs
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+        imageView.frame.size = image.size
+        scrollView.delegate = self
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 4.5
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+        rescaleAndCenterImageInScrollView(image: image)
+        
+        self.scrollView.isUserInteractionEnabled = true
     }
-
+    
+    // MARK: - Private funcs
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
+        if scrollView.zoomScale < scale {
+            scrollView.setZoomScale(scale, animated: true)
+        }
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let topInset = (visibleRectSize.height - newContentSize.height) / 2
+        scrollView.contentInset = UIEdgeInsets(top: topInset, left: 0, bottom: 0, right: 0)
+    }
+    
+    // MARK: IBActions
+    
+    @IBAction func didTapShareButton(_ sender: UIButton) {
+        let share = UIActivityViewController(
+            activityItems: [image],
+            applicationActivities: nil
+        )
+        present(share, animated: true, completion: nil)
+    }
+    
     @IBAction private func didTapBackButton() {
         dismiss(animated: true, completion: nil)
+    }
+}
+
+// MARK: - Extension
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
+    }
+    
+    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+        rescaleAndCenterImageInScrollView(image: image)
     }
 }
